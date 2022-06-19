4c1bd0dbe6ce784ad39e1e03a58c4d2f4026581d
Blake Sanie <blake@sanie.com>
2022-06-19 18:41:55 -0500

    just year

--- mlb/overTime.py
+++ mlb/overTime.py
@@ -350,7 +350,7 @@ def runsOverGame(
     else:
         starter = ""
 
-    message = f"{starter}{statsGame['winning_team']} ({winningScore}) > {statsGame['losing_team']} ({losingScore}){doubleHeader}\n\n{month}/{day}/{year[-2:]} @ {statsGame['venue_name']}, {getLocationByFullName(statsGame['home_name'])}\n\n{statsGame['winning_pitcher']} (W) > {statsGame['losing_pitcher']} (L)"
+    message = f"{starter}{statsGame['winning_team']} ({winningScore}) > {statsGame['losing_team']} ({losingScore}){doubleHeader}\n\n{month}/{day}/{year} @ {statsGame['venue_name']}, {getLocationByFullName(statsGame['home_name'])}\n\n{statsGame['winning_pitcher']} (W) > {statsGame['losing_pitcher']} (L)"
 
     if statsGame["save_pitcher"]:
         message += f"\n\nSave: statsGame['save_pitcher']"
